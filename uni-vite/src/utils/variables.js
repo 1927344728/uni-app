6a65e9da1f20990a258c00cd5fe153efbee27607
lizhao <1927344728@qq.com>
2025-07-23 23:41:40 +0800

    feat(apk): 接口联调

--- uni-vite/src/utils/variables.js
+++ uni-vite/src/utils/variables.js
@@ -5,9 +5,13 @@ const _hostname = location ? location.hostname : ''
 export const IS_PRODUCT_ENV = _hostname.includes('app.yizhao.com')
 export const IS_LOCALHOST_ENV = ['localhost', '192.168', '172.0.0.1', 'dev.lizhao.com'].some(s => _hostname.includes(s))
 
-export const SERVER_API_DOMAIN = import.meta.env.VITE_SERVE_HOST
-// export const SERVER_API_DOMAIN = 'https://[2409:8a28:8c4:2e52::5a4]'
-export const COS_ASSET_PATH = 'https://app-1259410276.cos.ap-shanghai.myqcloud.com/uni/'
+const VITE_SERVER_PROTOCOL = import.meta.env.VITE_SERVER_PROTOCOL
+export const SERVER_API_DOMAIN = import.meta.env.VITE_SERVER_HOST
+// export const SERVER_API_DOMAIN = `${VITE_SERVER_PROTOCOL}://[::1]`
+// export const SERVER_API_DOMAIN = `${VITE_SERVER_PROTOCOL}://[2409:8a28:8c4:2e52::79b]`
+// export const SERVER_API_DOMAIN = ${VITE_SERVER_PROTOCOL}://127.0.0.1`
+// export const SERVER_API_DOMAIN = `${VITE_SERVER_PROTOCOL}://192.168.31.146`
+export const COS_ASSET_PATH = `${VITE_SERVER_PROTOCOL}://app-1259410276.cos.ap-shanghai.myqcloud.com/uni/`
 export const WEB_DOMAIN = '/pages/'
 
 export const APP_CHANNEL = getAppChannel()
--- uni-vite/vite.config.js
+++ uni-vite/vite.config.js
@@ -1,16 +1,19 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import uni from '@dcloudio/vite-plugin-uni'
 
-export default defineConfig(async () => {
+export default defineConfig(async ({ mode }) => {
   const selfsigned = (await import('selfsigned')).default
-  const attrs = [{ name: 'commonName', value: 'localhost' }]
+  const attrs = [
+    { name: 'commonName', value: 'localhost' }
+  ]
   const pems = selfsigned.generate(attrs, { days: 365 })
-
+  const env = loadEnv(mode, process.cwd())
+  const protocol = env.VITE_SERVER_PROTOCOL
   return {
     plugins: [uni()],
     server: {
       port: 9000,
-      https: {
+      https: protocol === 'http' ? false : {
         key: pems.private,
         cert: pems.cert,
       },
